0cd2550c9551c0ace5105523f0ba234c71bd4261
felipesiqueira06 <felipe06quinho@gmail.com>
2025-06-21 15:28:32 -0300

    commit voltar btn

--- parte02/aula35/exercicioeven.js
+++ parte02/aula35/exercicioeven.js
@@ -1,3 +1,35 @@
-const caixa1 = document.getElementbyId("caixa1")
-const caixa2 = document.getElementbyId("caixa2")
-const btn = document.getElementbyId("btn_copiar")
+const caixa1 = document.getElementById("caixa1")
+const caixa2 = document.getElementById("caixa2")
+const btn = document.getElementById("btn_copiar")
+const btnc = document.getElementById("btn_voltar")
+const todosCurso = [...document.querySelectorAll(".curso")]
+
+todosCurso.map((el)=>{
+    el.addEventListener("click", (evt)=>{
+        const curso = evt.currentTarget
+        curso.classList.toggle("selecionado")
+    })
+})
+
+
+btn.addEventListener("click", ()=>{
+    const selecionados = [...document.querySelectorAll(".selecionado")]
+    selecionados.map((el)=>{
+        caixa2.appendChild(el)
+
+    })
+
+})
+
+btnc.addEventListener("click", ()=>{
+    const desmarcados = [...document.querySelectorAll(".c1")]
+    desmarcados.map((el)=>{
+        caixa1.appendChild(el)
+    })
+})
+
+
+
+/*toggle: se o elemento tem a classe selecionado ele remove a classe, se ja tem ele remove, ou seja, cliquei pela primeira vez add, cliquei dnv remove
+
+appendChild: Anexa o filho ao elemento selecionado, nesse caso no caixa2*/
