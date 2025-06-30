e3039fc2d1f89a0ac2e08cc66066f75092101008
felipesiqueira06 <felipe06quinho@gmail.com>
2025-06-30 20:27:48 -0300

    stopPropagation

--- parte02/aula36/stop.js
+++ parte02/aula36/stop.js
@@ -1,3 +1,16 @@
 const caixa1 = document.getElementById("caixa1")
 const btn_c1 = document.getElementById("c1")
+const cursos = [...document.querySelectorAll(".curso")]
+
+caixa1.addEventListener("click", () =>{
+    console.log("clicou")
+    
+} )
+
+cursos.map((el) => {
+    el.addEventListener("click", (evt) =>{
+    evt.stopPropagation()
+    
+} )
+})
 
