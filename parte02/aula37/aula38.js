9a93d067e63d5cff79fa5480d91ebc1e5415ae6b
felipesiqueira06 <felipe06quinho@gmail.com>
2025-06-30 21:24:30 -0300

    elementos DOM

---
+++ parte02/aula37/aula38.js
@@ -0,0 +1,6 @@
+const caixa1 = document.getElementById("caixa1")
+const novoel = document.createElement("div")
+caixa1.appendChild(novoel)
+novoel.innerHTML="C++"
+novoel.setAttribute("id", "c7")
+novoel.setAttribute("class", "curso c1")
