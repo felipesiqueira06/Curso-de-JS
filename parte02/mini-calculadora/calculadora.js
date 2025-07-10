30078e7bdbdf3e4abda4cc1a35d64d55727ea3db
felipesiqueira06 <felipe06quinho@gmail.com>
2025-07-10 14:43:49 -0300

    js do projeto calculadora

--- parte02/mini-calculadora/calculadora.js
+++ parte02/mini-calculadora/calculadora.js
@@ -8,3 +8,46 @@ const btn_adição = document.querySelector("#adi")
 const btn_sub = document.querySelector("#sub")
 const btn_mult = document.querySelector("#mult")
 const btn_divi = document.querySelector("#div")
+
+
+const op = [
+    
+    }, 
+    (valores) =>{
+        res = 0
+        for(v of valores){
+            res = valores[0]
+            res = valores[0] - valores[1]
+        }
+        resultadoFinal.value = res
+        
+
+    },
+    
+    (valores)=>{
+        res = 1
+        for(v of valores){
+            res *= v
+        }
+         resultadoFinal.value = res
+        
+    },
+
+    (valores)=>{
+        res = 1
+        for(v of valores){
+            valores[0] = res
+            res = res / valores[1]
+
+        }
+         resultadoFinal.value = res
+        
+    }
+]
+btn_adição.addEventListener("click", op[0])
+btn_sub.addEventListener("click", op[1])
+btn_mult.addEventListener("click", op[2])
+btn_divi.addEventListener("click", op[3])
+
+
+
