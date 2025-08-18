520953fa8889e8410b0c47c725754e0eadf3167c
Felipe Cunha Siqueira <felipe06quinho@gmail.com>
2025-08-18 14:35:27 -0300

    operacoes da calculadora

--- parte02/calculadora/calculadora.js
+++ parte02/calculadora/calculadora.js
@@ -4,21 +4,65 @@ const teclaRes = document.querySelector(".res")
 const display = document.querySelector(".display")
 const tON = document.getElementById("tON")
 const tlimpar = document.getElementById("tlimpar")
+let sinal = false 
+let decimal = false
+
 
 teclasNum.map((el)=>{
     el.addEventListener("click", (evt)=>{
-        display.innerHTML+= evt.target.innerHTML
-
+        sinal = false
+         if(evt.target.innerHTML == "."){
+            if(!decimal){
+            decimal = true
+            if(display.innerHTML == "0"){
+                display.innerHTML = "0."
+            }else{
+            display.innerHTML+= evt.target.innerHTML
+        }
+        }
+        }else{
+             if(display.innerHTML == "0"){
+            display.innerHTML =""
+        }
+      display.innerHTML+= evt.target.innerHTML 
+    }
+    
+        
     })
 })
 
 teclasOP.map((el)=>{
     el.addEventListener("click", (evt)=>{
-        display.innerHTML+= evt.target.innerHTML
+        decimal = false
+         if(!sinal){
+            sinal = true
+        if(display.innerHTML == "0"){
+            display.innerHTML =""
+        }
+        if(evt.target.innerHTML == "x"){
+            display.innerHTML += "*"
+        }else{
+            display.innerHTML+= evt.target.innerHTML
+        }
+    }
 
     })
 })
 
 tlimpar.addEventListener("click", (evt)=>{
-    display.innerHTML=0
+     sinal = false
+    decimal = false
+    display.innerHTML= "0"
+   
 })
+
+teclaRes.addEventListener("click", (evt)=>{
+     sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
+})
+
+
+
+
