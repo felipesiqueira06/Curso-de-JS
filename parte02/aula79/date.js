fff7df96cb09216fa23284dcbe52316c01482429
Felipe Cunha Siqueira <felipe06quinho@gmail.com>
2025-08-20 16:17:14 -0300

    relogio

--- parte02/aula79/date.js
+++ parte02/aula79/date.js
@@ -1,5 +1,10 @@
 const div_relogio = document.getElementById("div_relogio")
 const div_data = document.getElementById("div_data")
+const btn_ativar = document.getElementById("btn_ativar")
+const btn_parar = document.getElementById("btn_parar")
+const tmp_alarme = document.getElementById("tmp_alarme")
+const hora_alarme = document.getElementById("hora_alarme")
+const timer = document.getElementById("timer")
 
 
 const data = new Date()
