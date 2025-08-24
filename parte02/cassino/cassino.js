8c45b17edbb967f4efa7c75eb7167862a84ee2d1
Felipe Cunha Siqueira <felipe06quinho@gmail.com>
2025-08-24 18:40:32 -0300

    rodadas automaticas

--- parte02/cassino/cassino.js
+++ parte02/cassino/cassino.js
@@ -5,9 +5,12 @@ const div2 = document.getElementById("div2")
 const div3 = document.getElementById("div3")
 const rodadas = document.getElementById("rodadas")
 const main = document.getElementById("container")
+const play = document.getElementById("play")
+const conteudo = document.getElementById("conteudo")
 
 let opcao = false
 let div10, div15, div20 
+let totalRodadas = 0
 
 
 
@@ -39,15 +42,67 @@ rodadas.addEventListener("click", ()=>{
    main.appendChild(div15)
    main.appendChild(div20)
 
-   div10.addEventListener("click", () => {
-      alert("Você escolheu 10 rodadas")
+   const fecharopcao=()=>{
+    main.removeChild(div10)
+    main.removeChild(div15)
+    main.removeChild(div20)
 
-      opcao = false
+    opcao = false
+
+   }
+
+   div10.addEventListener("click", () => {
+    totalRodadas = 10  
+    play.style.display = "flex"
+      play.innerHTML = "Girar </br> 10 rodadas"
+      fecharopcao()
+      
+    })
+
+   
+
+    div15.addEventListener("click", () => {
+      totalRodadas =15
+        play.style.display = "flex"
+      play.innerHTML = "Girar </br> 15 rodadas"
+      fecharopcao()
+      
+    })
+
+    div20.addEventListener("click", () => {
+      totalRodadas = 20
+        play.style.display = "flex"
+      play.innerHTML = "Girar </br> 20 rodadas"
+      fecharopcao()
+       
+    })
+
+    play.addEventListener("click", ()=>{
+        let rodadaAtual = 1
+
+        function SortearNum(){
+            div_cassino.forEach((el)=>{
+                el.innerHTML = Math.floor(Math.random()*10)
+            })
+
+            btn.innerHTML= `Rodada atual: ${rodadaAtual}`
+
+            if(rodadaAtual < totalRodadas){
+                rodadaAtual++
+                setTimeout(SortearNum, 500)
+            } else if(div1.innerHTML == div2.innerHTML && div1.innerHTML == div3.innerHTML){
+               window.alert("GANHOU!")
+                } else{
+                    alert("Rodadas expiradas")
+                }
+        }
+
+        SortearNum()
     })
 
    opcao = true
+
     }else{
-        
     main.removeChild(div10)
     main.removeChild(div15)
     main.removeChild(div20)
