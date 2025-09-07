a8b6fa52ca1d346c7530c1d24657fe3975ee6314
Felipe Cunha Siqueira <felipe06quinho@gmail.com>
2025-09-07 19:43:31 -0300

    exercicio Bolinhas

--- parte02/aula82 bolinhas/classes.js
+++ parte02/aula82 bolinhas/classes.js
@@ -2,7 +2,7 @@ const palco = document.getElementById("palco")
 const num_objetos = document.getElementById("num_objetos")
 const txt_qtde = document.getElementById("txt_qtde")
 const btn_add = document.getElementById("btn_add")
-const btn_remover = document.getElementById("btn_removers")
+const btn_remover = document.getElementById("btn_remover")
 
 let larguraPalco = palco.offsetWidth
 let alturaPalco = palco.offsetHeight
@@ -51,13 +51,36 @@ class Bola{
         const div = document.createElement("div")
         div.setAttribute("id",this.id)
         div.setAttribute("class","bola")
-        div.setAttribute("style", `left:${this.px};top:${this.py};width${this.tam};height:${this.tam};background-color:rgb(${this.r},${this.g},${this.b})`)
+        div.setAttribute("style", `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
         this.palco.appendChild(div)
 
 
     }
 
+    controle_bordas=()=>{
+        if(this.px+this.tam >= larguraPalco){
+            this.dirx = -1
+        } else if(this.px <= 0){
+            this.dirx =1
+        }
+
+        if(this.py+this.tam >= alturaPalco){
+            this.diry = -1
+        } else if(this.py <= 0){
+            this.diry =1
+        }
+
+    }
+
     controlar=()=>{
+        this.controle_bordas()
+        this.px+= this.dirx*this.velx
+        this.py+= this.diry*this.vely
+        this.eu.setAttribute("style", `left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
+        if((this.px > larguraPalco)||(this.py > alturaPalco)){
+            this.remover()
+        }
+
     }
 
     
@@ -72,12 +95,15 @@ btn_add.addEventListener("click", (evt)=>{
     const qtde = txt_qtde.value
     for(let i =0;i<qtde;i++){
 
+        bolas.push(new Bola (bolas,palco))
+
 
     }
 })
 
 btn_remover.addEventListener("click", (evt)=>{
     bolas.map((b)=>{
+        b.remover()
 
     })
 })
